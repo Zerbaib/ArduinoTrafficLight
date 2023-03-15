038d5af812caec1e4b427e548698c2568eabb920
Zerbaib <hugo.hennetin@proton.me>
2023-03-15 10:23:33 +0100

    done the code

--- main.ino
+++ main.ino
@@ -27,5 +27,34 @@ void setup() {
 }
 
 void loop() {
-    // code
+    digitalWrite(led1, HIGH);
+    digitalWrite(led4, HIGH);
+
+    digitalWrite(led9, HIGH);
+    digitalWrite(led12, HIGH);
+    delay(4000);
+    digitalWrite(led1, LOW);
+    digitalWrite(led4, LOW);
+
+    digitalWrite(led2, HIGH);
+    digitalWrite(led5, HIGH);
+    delay(1000);
+    digitalWrite(led2, LOW);
+    digitalWrite(led5, LOW);
+
+    digitalWrite(led7, HIGH);
+    digitalWrite(led10, HIGH);
+    digitalWrite(led9, LOW);
+    digitalWrite(led12, LOW);
+
+    digitalWrite(led3, HIGH);
+    digitalWrite(led6, HIGH);
+    delay(4000);
+    digitalWrite(led8, HIGH);
+    digitalWrite(led11, HIGH);
+    digitalWrite(led7, LOW);
+    digitalWrite(led10, LOW);
+    delay(1000);
+    digitalWrite(led3, LOW);
+    digitalWrite(led6, LOW);
 }
