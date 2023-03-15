f15ea0aea81be9233358375158b1604275633b79
Zerbaib <hugo.hennetin@proton.me>
2023-03-15 10:06:04 +0100

    base of the code

---
+++ main.ino
@@ -0,0 +1,20 @@
+const int led1 = 2;
+const int led2 = 3;
+const int led3 = 4;
+const int led4 = 5;
+const int led5 = 6;
+const int led6 = 7;
+const int led7 = 8;
+const int led8 = 9;
+const int led9 = 10;
+const int led10 = 11;
+const int led11 = 12;
+const int led12 = 13;
+
+void setup() {
+    //code 
+}
+
+void loop() {
+    // code
+}
