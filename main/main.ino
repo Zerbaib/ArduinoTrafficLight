0d29f958b775922b0ddb4a1cecbcf3ad17ffcf6b
Zerbaib <hugo.hennetin@proton.me>
2023-04-12 16:26:55 +0200

    rename variable for green

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,14 @@
 // set the var
-const int v1 = 2;
+const int g1 = 2;
 const int o1 = 3;
 const int r1 = 4;
-const int v2 = 5;
+const int g2 = 5;
 const int o2 = 6;
 const int r2 = 7;
-const int v3 = 8;
+const int g3 = 8;
 const int o3 = 9;
 const int r3 = 10;
-const int v4 = 11;
+const int g4 = 11;
 const int o4 = 12;
 const int r4 = 13;
 
@@ -16,16 +16,16 @@ const int pV = 4000;
 const int pO = 1000;
 
 void setup() {
-    pinMode(v1, OUTPUT);
+    pinMode(g1, OUTPUT);
     pinMode(o1, OUTPUT);
     pinMode(r1, OUTPUT);
-    pinMode(v2, OUTPUT);
+    pinMode(g2, OUTPUT);
     pinMode(o2, OUTPUT);
     pinMode(r2, OUTPUT);
-    pinMode(v3, OUTPUT);
+    pinMode(g3, OUTPUT);
     pinMode(o3, OUTPUT);
     pinMode(r3, OUTPUT);
-    pinMode(v4, OUTPUT);
+    pinMode(g4, OUTPUT);
     pinMode(o4, OUTPUT);
     pinMode(r4, OUTPUT);
     Serial.begin(9600);
@@ -48,8 +48,8 @@ void pV1(){
     digitalWrite(o3, LOW); // orange 3 off
     digitalWrite(o4, LOW); // orange 4 off
     Serial.println("Orange fire 3/4 off");
-    digitalWrite(v1, HIGH); // green 1 on
-    digitalWrite(v2, HIGH); // green 2 on
+    digitalWrite(g1, HIGH); // green 1 on
+    digitalWrite(g2, HIGH); // green 2 on
     Serial.println("Green fire 1/2 on");
     delay(pV);
 }
@@ -63,8 +63,8 @@ void pV2(){
     digitalWrite(o1, LOW); // orange 1 off
     digitalWrite(o2, LOW); // orange 2 off
     Serial.println("Orange fire 1/2 off");
-    digitalWrite(v3, HIGH); // green 3 on
-    digitalWrite(v4, HIGH); // green 4 on
+    digitalWrite(g3, HIGH); // green 3 on
+    digitalWrite(g4, HIGH); // green 4 on
     Serial.println("Green fire 3/4 on");
     delay(pV);
 }
@@ -72,8 +72,8 @@ void pO1(){
     digitalWrite(o1, HIGH); // orange 1 on
     digitalWrite(o2, HIGH); // orange 2 on
     Serial.println("Orange fire 1/2 on");
-    digitalWrite(v1, LOW); // green 1 off
-    digitalWrite(v2, LOW); // green 2 off
+    digitalWrite(g1, LOW); // green 1 off
+    digitalWrite(g2, LOW); // green 2 off
     Serial.println("Green fire 1/2 off");
     delay(pO);
 }
@@ -81,8 +81,8 @@ void pO2(){
     digitalWrite(o3, HIGH); // orange 3 on
     digitalWrite(o4, HIGH); // orange 4 on
     Serial.println("Orange fire 3/4 on");
-    digitalWrite(v3, LOW); // green 3 off
-    digitalWrite(v4, LOW); // green 4 off
+    digitalWrite(g3, LOW); // green 3 off
+    digitalWrite(g4, LOW); // green 4 off
     Serial.println("Green fire 3/4 off");
     delay(pO);
 }
