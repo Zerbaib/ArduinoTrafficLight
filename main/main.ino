29fcc66c2f1543691e12c300436025543ad7adf9
Zerbaib <hugo.hennetin@proton.me>
2023-04-12 16:24:17 +0200

    create fonction

--- main/main.ino
+++ main/main.ino
@@ -32,6 +32,13 @@ void setup() {
 }
 
 void loop() {
+    pV1();
+    pO1();
+    pV2();
+    pO2();
+}
+
+void pV1(){
     digitalWrite(r1, LOW); // red 1 off
     digitalWrite(r2, LOW); // red 2 off
     Serial.println("Red fire 1/2 off");
@@ -45,15 +52,8 @@ void loop() {
     digitalWrite(v2, HIGH); // green 2 on
     Serial.println("Green fire 1/2 on");
     delay(pV);
-
-    digitalWrite(o1, HIGH); // orange 1 on
-    digitalWrite(o2, HIGH); // orange 2 on
-    Serial.println("Orange fire 1/2 on");
-    digitalWrite(v1, LOW); // green 1 off
-    digitalWrite(v2, LOW); // green 2 off
-    Serial.println("Green fire 1/2 off");
-    delay(pO);
-    
+}
+void pV2(){
     digitalWrite(r1, HIGH); // red 1 on
     digitalWrite(r2, HIGH); // red 2 on
     Serial.println("Red fire 1/2 on");
@@ -67,7 +67,17 @@ void loop() {
     digitalWrite(v4, HIGH); // green 4 on
     Serial.println("Green fire 3/4 on");
     delay(pV);
-    
+}
+void pO1(){
+    digitalWrite(o1, HIGH); // orange 1 on
+    digitalWrite(o2, HIGH); // orange 2 on
+    Serial.println("Orange fire 1/2 on");
+    digitalWrite(v1, LOW); // green 1 off
+    digitalWrite(v2, LOW); // green 2 off
+    Serial.println("Green fire 1/2 off");
+    delay(pO);
+}
+void pO2(){
     digitalWrite(o3, HIGH); // orange 3 on
     digitalWrite(o4, HIGH); // orange 4 on
     Serial.println("Orange fire 3/4 on");
@@ -75,5 +85,4 @@ void loop() {
     digitalWrite(v4, LOW); // green 4 off
     Serial.println("Green fire 3/4 off");
     delay(pO);
-
 }
